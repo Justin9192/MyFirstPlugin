a9fdaa88d09ce631af98b7e1d17dca88bb3f41e8
Justin9192 <61888788+Justin9192@users.noreply.github.com>
2023-06-23 22:21:20 -0700

    Original Layout and Math Test
    Adding components, testing a new waveform

--- src/MyFirstModule.cpp
+++ src/MyFirstModule.cpp
@@ -17,6 +17,10 @@ struct MyFirstModule : Module {
 		SINE_OUTPUT,
 		OUTPUTS_LEN
 	};
+	enum OutpudId0 {
+		SAW_OUTPUT,
+		OUTPUTS_LEN0,
+	};
 	enum LightId {
 		BLINK_LIGHT,
 		LIGHTS_LEN
@@ -27,6 +31,7 @@ struct MyFirstModule : Module {
 		configParam(PITCH_PARAM, 0.f, 1.f, 0.f, "");
 		configInput(PITCH_INPUT, "");
 		configOutput(SINE_OUTPUT, "");
+		configOutput(SAW_OUTPUT, "");
 	}
 
 	void process(const ProcessArgs& args) override {
@@ -44,9 +49,12 @@ struct MyFirstModule : Module {
 
 		// Compute the sine output
 		float sine = std::sin(2.f * M_PI * phase);
+		// trying to add saw
+		float saw = std::sin((4.f * M_PI * phase));
 		// Audio signals are typically +/-5V
 		// https://vcvrack.com/manual/VoltageStandards
 		outputs[SINE_OUTPUT].setVoltage(5.f * sine);
+		outputs[SAW_OUTPUT].setVoltage(5.f * saw);
 
 		// Blink light at 1Hz
 		blinkPhase += args.sampleTime;
@@ -60,7 +68,7 @@ struct MyFirstModule : Module {
 struct MyFirstModuleWidget : ModuleWidget {
 	MyFirstModuleWidget(MyFirstModule* module) {
 		setModule(module);
-		setPanel(createPanel(asset::plugin(pluginInstance, "res/MyFirstModule.svg")));
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/MyFIrstModule.svg")));
 
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
@@ -72,6 +80,7 @@ struct MyFirstModuleWidget : ModuleWidget {
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 77.478)), module, MyFirstModule::PITCH_INPUT));
 
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 108.713)), module, MyFirstModule::SINE_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(13.50, 85.55)), module, MyFirstModule::SAW_OUTPUT));
 
 		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(15.24, 25.81)), module, MyFirstModule::BLINK_LIGHT));
 	}
